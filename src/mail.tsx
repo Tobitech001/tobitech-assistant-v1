e926a87383f6d09befa01b306797cf4dd381305b
Tobitech001 <moshoodabdulmujibtobiloba@gmail.com>
2025-07-27 10:32:25 +0100

    Add mail.tsx

---
+++ src/mail.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Mail() {
+  return (
+    <div>
+      <p>Hello, I am your TOBI TECH Assistant. Let’s start creating your content for today. 🚀</p>
+    </div>
+  );
+}
